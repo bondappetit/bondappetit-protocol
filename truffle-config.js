80da1e72e899d196e2a1ca7a54ae640ab2132fa5
Vladislav Komissarov <mastyf@gmail.com>
2020-11-13 15:25:27 +0300

    "Bond" ran out of gas (using a value you set in your network config or deployment parameters.)    * Block limit:  8000000 (0x7a1200)    * Gas sent:     3000000 (0x2dc6c0)

--- truffle-config.js
+++ truffle-config.js
@@ -28,26 +28,26 @@ module.exports = {
     ropsten: {
       provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY),
       network_id: 3,
-      gas: 3000000,
+      gas: 8000000,
       gasPrice: 10000000000
     },
     kovan: {
       provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://kovan.infura.io/v3/" + process.env.INFURA_API_KEY),
       network_id: 42,
-      gas: 3000000,
+      gas: 8000000,
       gasPrice: 10000000000
     },
     rinkeby: {
       provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://rinkeby.infura.io/v3/" + process.env.INFURA_API_KEY),
       network_id: 4,
-      gas: 3000000,
+      gas: 8000000,
       gasPrice: 10000000000
     },
     // main ethereum network(mainnet)
     main: {
       provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY),
       network_id: 1,
-      gas: 3000000,
+      gas: 8000000,
       gasPrice: 10000000000
     }
   },
