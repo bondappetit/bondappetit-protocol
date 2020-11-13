74ec4970533d2f12cbbdd9eca4d9e1729281c910
Vladislav Komissarov <mastyf@gmail.com>
2020-11-13 15:11:55 +0300

    github actions and infura integration

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,7 @@
 const networks = require("./networks");
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+require('dotenv').config();
 
 module.exports = {
   compilers: {
@@ -13,16 +16,39 @@ module.exports = {
   },
   networks: {
     development: {
-      host: "127.0.0.1",
+      host: 'localhost',
       port: 8545,
-      network_id: "999",
-      from: networks.development.accounts.Governor.address,
+      network_id: '*' // Match any network id
     },
+    // testnets
+    // properties
+    // network_id: identifier for network based on ethereum blockchain. Find out more at https://github.com/ethereumbook/ethereumbook/issues/110
+    // gas: gas limit
+    // gasPrice: gas price in gwei
     ropsten: {
-      host: "10.49.69.27",
-      port: 8545,
-      network_id: "3",
-      from: networks.ropsten.accounts.Governor.address,
+      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY),
+      network_id: 3,
+      gas: 3000000,
+      gasPrice: 10000000000
     },
+    kovan: {
+      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://kovan.infura.io/v3/" + process.env.INFURA_API_KEY),
+      network_id: 42,
+      gas: 3000000,
+      gasPrice: 10000000000
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://rinkeby.infura.io/v3/" + process.env.INFURA_API_KEY),
+      network_id: 4,
+      gas: 3000000,
+      gasPrice: 10000000000
+    },
+    // main ethereum network(mainnet)
+    main: {
+      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY),
+      network_id: 1,
+      gas: 3000000,
+      gasPrice: 10000000000
+    }
   },
 };
