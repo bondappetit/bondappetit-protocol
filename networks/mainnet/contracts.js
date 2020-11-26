8f08637310be043e67310ae41e8f9eeb0c697d62
artur <amamedbekov@tokenomica.com>
2020-11-26 15:02:22 +0300

    price oracle abi to config

--- networks/mainnet/contracts.js
+++ networks/mainnet/contracts.js
@@ -3,7 +3,27 @@ module.exports = {
     address: "0x922018674c12a7F0D394ebEEf9B58F186CdE13c1",
     name: "UniswapAnchoredView",
     voting: false,
-    abi: [],
+    abi: [
+      {
+        inputs: [
+          {
+            internalType: "string",
+            name: "symbol",
+            type: "string",
+          },
+        ],
+        name: "price",
+        outputs: [
+          {
+            internalType: "uint256",
+            name: "",
+            type: "uint256",
+          },
+        ],
+        stateMutability: "view",
+        type: "function",
+      },
+    ],
   },
   UniswapV2Router02: {
     address: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
--- networks/ropsten/contracts.js
+++ networks/ropsten/contracts.js
@@ -3,7 +3,27 @@ module.exports = {
     address: "0xbef4e076a995c784be6094a432b9ca99b7431a3f",
     name: "UniswapAnchoredView",
     voting: false,
-    abi: [],
+    abi: [
+      {
+        inputs: [
+          {
+            internalType: "string",
+            name: "symbol",
+            type: "string",
+          },
+        ],
+        name: "price",
+        outputs: [
+          {
+            internalType: "uint256",
+            name: "",
+            type: "uint256",
+          },
+        ],
+        stateMutability: "view",
+        type: "function",
+      },
+    ],
   },
   UniswapV2Router02: {
     address: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
