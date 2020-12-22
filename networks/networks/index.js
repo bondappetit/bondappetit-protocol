42955e72b99e4edc6b4211dbdf5a7e3073abfb0a
Vladislav Komissarov <mastyf@gmail.com>
2020-12-22 16:20:28 +0300

    remove wrong generated code

--- networks/networks/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  development: require("./development"),
-  "development-fork": require("./development"),
-  ropsten: require("./ropsten"),
-  "ropsten-fork": require("./ropsten"),
-  main: require("./mainnet"),
-  "main-fork": require("./mainnet"),
-};
--- networks/networks/mainnet/accounts.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  Governor: {
-    address: "0x87aa18a47EeE34F47187159Ba3431aF143a5ceA8",
-    name: "Governor",
-  },
-};
