5f88e5cebcba44278073ffe04d07124ea2a7a47e
Vladislav Komissarov <mastyf@gmail.com>
2021-02-01 11:03:57 +0300

    new manual deploy for ropsten

--- networks/ropsten/gen/assets.js
+++ networks/ropsten/gen/assets.js
@@ -1,22 +1,15 @@
 module.exports = {
     "Governance": {
-        "address": "0xd1bBEe39864513C4059Aca4340de74A47C9D1572",
+        "address": "0x60a7Be2759f260Ee2AF5f97ff36009Ce02925d67",
         "name": "BondAppetit Governance",
         "symbol": "BAG",
         "decimals": 18,
         "investing": false
     },
     "Stable": {
-        "address": "0xda698F2C2A51CeE4059D15a2F8fC8f5AaFA4F43c",
+        "address": "0x66730BefAfB8af177966bdf295796802c042D531",
         "name": "Appetite USD",
         "symbol": "USDp",
-        "decimals": 18,
-        "investing": false
-    },
-    "USDN": {
-        "address": "0xA18603F278F8147Ba8372aC19710C583563c13E1",
-        "name": "Neutrino USD",
-        "symbol": "USDN",
         "decimals": 18,
         "investing": false
     }
