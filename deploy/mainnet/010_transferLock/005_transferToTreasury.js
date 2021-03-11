dfa07d9051af3f7bfdbb9411b625b8f4e739bd7b
artur <amamedbekov@tokenomica.com>
2021-03-11 11:45:55 +0300

    audit reward

--- deploy/mainnet/010_transferLock/005_transferToTreasury.js
+++ deploy/mainnet/010_transferLock/005_transferToTreasury.js
@@ -3,7 +3,7 @@ const {migration, bn} = require("../../../utils/deploy");
 module.exports = migration("GovernanceToken.transferToTreasury", async (d) => {
   const [treasury] = await d.deployed("Treasury");
   const govTotalSupply = await d.call("GovernanceToken", "totalSupply", []);
-  const amount = bn(govTotalSupply).div(bn(10000)).mul(bn(5902)).toString(); // 59.02%
+  const amount = bn(govTotalSupply).div(bn(10000)).mul(bn(5902)).sub(bn(1823)).toString(); // 59.02% - audit
 
   await d.send("GovernanceToken", "transfer", [treasury.address, amount]);
 });
