cbd70ef5631a30633417b634b2fe43a90498f698
mastyf <mastyf@https://users.noreply.github.com/>
2021-04-08 14:19:43 +0000

    new build 1.9.10 on 2021-04-08 14:19:43

--- networks/ropsten/gen/assets.js
+++ networks/ropsten/gen/assets.js
@@ -1,13 +1,13 @@
 module.exports = {
     "Governance": {
-        "address": "0xdeF4517d8762527B11B2dac65F89DD3DE05c5911",
+        "address": "0xE7afDD9D557e70F6Ed22f764a49a7F7C183BC5AF",
         "name": "BondAppetit Governance",
         "symbol": "BAG",
         "decimals": 18,
         "investing": false
     },
     "Stable": {
-        "address": "0x7d9E349820507a8eF49eE98d928e8dea732AB9c0",
+        "address": "0x7887ad7C7C42E015c552ebd90b4AC75823015383",
         "name": "Appetite USD",
         "symbol": "USDap",
         "decimals": 18,
