164af35827132f4e8daaeaf53e0b801debe42d3e
artur <amamedbekov@tokenomica.com>
2021-04-28 15:41:45 +0300

    use new infura project for dev

--- hardhat.config.js
+++ hardhat.config.js
@@ -19,11 +19,6 @@ module.exports = {
         mnemonic: process.env.MNENOMIC,
         unlocked: ["0x876A207aD9f6f0fA2C58A7902B2E7568a41c299f"],
       },
-      /*
-     accounts: [
-       '0x3490e2cca2c8c14a5b7b2241ca4c13b5148d36e2e48653aef6751412d5a901ed'
-     ]
-     */
     },
     ropsten: {
       url: "https://ropsten.bondappetit.io",
@@ -35,7 +30,7 @@ module.exports = {
       },
     },
     mainnet: {
-      url: "https://mainnet.infura.io/v3/03d22d3114e54a6dbab0cb1c6163b48a",
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
       chainId: 1,
       from: networks.main.accounts.Governor.address,
       gasPrice: parseInt(networks.main.gasPrice, 10),
