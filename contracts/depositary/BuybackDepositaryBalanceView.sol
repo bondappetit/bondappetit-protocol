1bbe963ace8bedab30d98d8d2af5e1fd5d49800e
artur <amamedbekov@tokenomica.com>
2021-05-28 12:11:48 +0300

    audit fixes

--- contracts/depositary/BuybackDepositaryBalanceView.sol
+++ contracts/depositary/BuybackDepositaryBalanceView.sol
@@ -8,7 +8,6 @@ import "@openzeppelin/contracts/math/Math.sol";
 import "../utils/OwnablePausable.sol";
 import "./IDepositaryBalanceView.sol";
 import "../Issuer.sol";
-import "../Treasury.sol";
 
 contract BuybackDepositaryBalanceView is IDepositaryBalanceView, OwnablePausable {
     using Math for uint256;
@@ -16,28 +15,29 @@ contract BuybackDepositaryBalanceView is IDepositaryBalanceView, OwnablePausable
     using SafeERC20 for ERC20;
 
     /// @notice Balance decimals.
-    uint256 public override decimals = 18;
+    uint256 public override constant decimals = 18;
 
     /// @notice Address of product token.
-    ERC20 public product;
+    ERC20 public immutable product;
 
     /// @notice Address of issuer contract.
     Issuer public issuer;
 
-    /// @notice An event thats emitted when an issuer contract address changed.
+    /// @notice An event that is emitted when an issuer contract address changed.
     event IssuerChanged(address newIssuer);
 
-    /// @notice An event thats emitted when an account buyback stable token.
-    event Buyback(address customer, uint256 amount, uint256 buy);
+    /// @notice An event that is emitted when an account buyback stable token.
+    event Buyback(address indexed customer, uint256 amount, uint256 buy);
 
     /**
      * @param _issuer Issuer contract address.
      * @param _product Product token address.
      */
     constructor(address _issuer, address _product) public {
+        require(ERC20(_product).decimals() <= decimals, "BuybackDepositaryBalanceView::constructor: invalid decimals of product token");
+
         issuer = Issuer(_issuer);
         product = ERC20(_product);
-        require(product.decimals() <= decimals, "BuybackDepositaryBalanceView::constructor: invalid decimals of product token");
     }
 
     /**
@@ -45,15 +45,17 @@ contract BuybackDepositaryBalanceView is IDepositaryBalanceView, OwnablePausable
      * @param _issuer New issuer contract address.
      */
     function changeIssuer(address _issuer) external onlyOwner {
+        require(_issuer != address(0), "BuybackDepositaryBalanceView::changeIssuer: invalid issuer address");
+
         issuer = Issuer(_issuer);
         emit IssuerChanged(_issuer);
     }
 
     /**
      * @dev Transfer token to recipient.
-     * @param token Transfered token.
+     * @param token Transferred token.
      * @param recipient Address of recipient.
-     * @param amount Amount of transfered token.
+     * @param amount Amount of transferred token.
      */
     function _transfer(
         ERC20 token,
@@ -68,7 +70,7 @@ contract BuybackDepositaryBalanceView is IDepositaryBalanceView, OwnablePausable
     /**
      * @notice Transfer product token.
      * @param recipient Address of recipient.
-     * @param amount Amount of transfered token.
+     * @param amount Amount of transferred token.
      */
     function transferProductToken(address recipient, uint256 amount) external onlyOwner {
         _transfer(product, recipient, amount);
@@ -77,7 +79,7 @@ contract BuybackDepositaryBalanceView is IDepositaryBalanceView, OwnablePausable
     /**
      * @notice Transfer stable token.
      * @param recipient Address of recipient.
-     * @param amount Amount of transfered token.
+     * @param amount Amount of transferred token.
      */
     function transferStableToken(address recipient, uint256 amount) external onlyOwner {
         _transfer(ERC20(issuer.stableToken()), recipient, amount);
@@ -90,17 +92,21 @@ contract BuybackDepositaryBalanceView is IDepositaryBalanceView, OwnablePausable
     function buy(uint256 amount) external whenNotPaused {
         uint256 productAmount = amount.div(10**(decimals.sub(product.decimals())));
         require(productAmount > 0, "BuybackDepositaryBalanceView::buy: invalid product amount");
-        require(productAmount >= product.balanceOf(address(this)), "BuybackDepositaryBalanceView::buy: product amount exceeds balance");
+        require(productAmount <= product.balanceOf(address(this)), "BuybackDepositaryBalanceView::buy: product amount exceeds balance");
 
         ERC20 stableToken = ERC20(issuer.stableToken());
         stableToken.safeTransferFrom(_msgSender(), address(this), amount);
         product.safeTransfer(_msgSender(), productAmount);
 
-        uint256 issuerInbalance = stableToken.totalSupply().sub(issuer.balance());
-        uint256 burningAmount = issuerInbalance.min(stableToken.balanceOf(address(this)));
-        if (burningAmount > 0) {
-            stableToken.safeTransfer(address(issuer), burningAmount);
-            issuer.rebalance();
+        uint256 stableTotalSupply = stableToken.totalSupply();
+        uint256 collateralBalance = issuer.balance();
+        if (stableTotalSupply > collateralBalance) {
+            uint256 issuerInbalance = stableTotalSupply.sub(collateralBalance);
+            uint256 burningAmount = issuerInbalance.min(stableToken.balanceOf(address(this)));
+            if (burningAmount > 0) {
+                stableToken.safeTransfer(address(issuer), burningAmount);
+                issuer.rebalance();
+            }
         }
 
         emit Buyback(_msgSender(), amount, productAmount);
