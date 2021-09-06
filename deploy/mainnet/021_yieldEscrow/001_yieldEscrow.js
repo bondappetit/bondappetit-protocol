751c64273a1080c42f349be3b3f635d32e1e5bfa
artur <amamedbekov@tokenomica.com>
2021-09-06 18:47:03 +0300

    yield escrow deploy scripts

---
+++ deploy/mainnet/021_yieldEscrow/001_yieldEscrow.js
@@ -0,0 +1,12 @@
+const {migration} = require("../../../utils/deploy");
+
+module.exports = migration("YieldEscrow", async (d) => {
+  const [gov] = await d.deployed("GovernanceToken");
+
+  await d.deploy("VoteDelegator");
+  const [voteDelegatorPrototype] = await d.deployed("VoteDelegator");
+
+  await d.deploy("YieldEscrow", {
+    args: [gov.address, voteDelegatorPrototype.address],
+  });
+});
---
+++ deploy/mainnet/021_yieldEscrow/003_transferOwnership.js
@@ -0,0 +1,23 @@
+const {migration} = require("../../../utils/deploy");
+
+module.exports = migration("YieldEscrow.transferOwnership", async (d) => {
+  const [timelock] = await d.deployed("Timelock");
+  const [
+    profitDistributor3,
+    profitDistributor6,
+    profitDistributor12,
+  ] = await d.deployed(
+    "ProfitDistributor3",
+    "ProfitDistributor6",
+    "ProfitDistributor12"
+  );
+
+  await d.send("YieldEscrow", "allowTransfer", [profitDistributor3.address]);
+  await d.send("YieldEscrow", "allowTransfer", [profitDistributor6.address]);
+  await d.send("YieldEscrow", "allowTransfer", [profitDistributor12.address]);
+
+  await d.send("ProfitDistributor3", "transferOwnership", [timelock.address]);
+  await d.send("ProfitDistributor6", "transferOwnership", [timelock.address]);
+  await d.send("ProfitDistributor12", "transferOwnership", [timelock.address]);
+  await d.send("YieldEscrow", "transferOwnership", [timelock.address]);
+});
