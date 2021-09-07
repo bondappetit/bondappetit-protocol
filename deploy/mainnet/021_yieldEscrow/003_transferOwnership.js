5f478282ad855220f218685ea4c92596279867b7
artur <amamedbekov@tokenomica.com>
2021-09-07 10:46:18 +0300

    fix

--- deploy/mainnet/021_yieldEscrow/003_transferOwnership.js
+++ deploy/mainnet/021_yieldEscrow/003_transferOwnership.js
@@ -16,8 +16,11 @@ module.exports = migration("YieldEscrow.transferOwnership", async (d) => {
   await d.send("YieldEscrow", "allowTransfer", [profitDistributor6.address]);
   await d.send("YieldEscrow", "allowTransfer", [profitDistributor12.address]);
 
+  await d.send("ProfitDistributor3", "changeRewardsDistribution", [timelock.address]);
   await d.send("ProfitDistributor3", "transferOwnership", [timelock.address]);
+  await d.send("ProfitDistributor6", "changeRewardsDistribution", [timelock.address]);
   await d.send("ProfitDistributor6", "transferOwnership", [timelock.address]);
+  await d.send("ProfitDistributor12", "changeRewardsDistribution", [timelock.address]);
   await d.send("ProfitDistributor12", "transferOwnership", [timelock.address]);
   await d.send("YieldEscrow", "transferOwnership", [timelock.address]);
 });
